733665fecfd34445d130b7a30ed0fbb45299e8a9
unknown <deepblue@inha.ac.kr>
2025-02-11 10:35:47 +0900

    Add day01.py file

---
+++ day02.py
@@ -0,0 +1,11 @@
+dan = input("Input dan : ")
+for i in range(1, 10, 1):
+        print(f"{dan} * {i} = {dan*i}")
+
+
+
+# for dan in range(2, 10, 1): #반복문 중괄호를 안쓰고 들여쓰끼
+#     for i in range(1, 10, 1):
+#         print(f"{dan} * {i} = {dan*i}")
+
+
