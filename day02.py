798e74ef24c3f8d29e11eeacbd78cf673c5edef9
unknown <deepblue@inha.ac.kr>
2025-02-11 11:06:54 +0900

    upload day02.py

--- day02.py
+++ day02.py
@@ -1,11 +1,31 @@
-dan = input("Input dan : ")
-for i in range(1, 10, 1):
-        print(f"{dan} * {i} = {dan*i}")
+# dan = input("Input dan : ")
+# for i in range(1, 10, 1):
+#         print(f"{dan} * {i} = {dan*i}")
 
 
 
 # for dan in range(2, 10, 1): #반복문 중괄호를 안쓰고 들여쓰끼
 #     for i in range(1, 10, 1):
 #         print(f"{dan} * {i} = {dan*i}")
+
+n = int(input("Input number : "))
+is_prime = True
+
+if n >= 2:
+        count = 0
+
+        for i in range(2, n):
+                if n % i == 0:
+                    count = count + 1
+                    break
+else:
+    is_prime = False
+
+# if count == 0:
+        print(f"{n} is prime number")
+if is_prime:
+
+else:
+        print(f"{n} is NOT prime number!")
 
 
