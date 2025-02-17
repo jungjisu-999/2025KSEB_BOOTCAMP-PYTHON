c8af9112a370bdf8bf919ecb1277dd6a1b3d5c4c
unknown <jisu1008@inha.edu>
2025-02-17 14:06:39 +0900

    v0.7 day06.py

--- day06.py
+++ day06.py
@@ -1,7 +1,43 @@
 #O(1)
-
+#시간 복잡도
 n = int(input())
 print(n * (n+1) // 2)
+
+
+
+
+def print_poly(f_x) -> str:
+    # term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+fx = [2, 3, 4, 0, -9]
+
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
 
 
 
