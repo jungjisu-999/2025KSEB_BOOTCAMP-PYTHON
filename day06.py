ca44efefa763f2f21e168c14eb027d679b46e24d
unknown <jisu1008@inha.edu>
2025-02-17 15:13:29 +0900

    v0.9 day06.py memo

--- day06.py
+++ day06.py
@@ -1,48 +1,24 @@
-#O(1)
-#시간 복잡도
+memo = dict()
+
+
+def fibonacci_recursion(n) -> int:...
+
+def fibonacci_loop(n) -> int...
+
+def fibonacci_memo(n) -> int:
+    if n in memo: # 딕셔너리에 이미 계산된 결과가 있으면 그 값을 리턴
+        return memo[n]
+    elif n <= 1: # 0이나 1이 오면 그 값을 바로 리턴
+        return n
+    else:
+        memo[n] = fibonacci_memo(n-2) + fibonacci_memo(n-1) #딕셔너리에 계산된 결과 값이 없을 경우 딕셔너리에 추가
+        return memo[n]
+
+
 n = int(input())
-print(n * (n+1) // 2)
-
-
-
-
-def print_poly(f_x) -> str:
-    # term = len(f_x) - 1
-    poly_expression = "f(x) = "
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-
-        if coefficient >= 0:
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
-        term = term - 1
-
-    return poly_expression
-
-
-def calculation_poly(x_value, f_x) -> int:...
-    return_value = 0
-    # term = len(f_x) - 1
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        term = t_x[i]
-
-        if coefficient >= 0:
-            poly_expression + poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term}'
-        return_value += coefficient * pow(x_value, term)
-        # term = term - 1
-
-    return poly_expression
-
-
-fx = [2, 3, 4, 0, -9]
-
-if __name__ == "__main__":
-    print(print_poly(fx))
-    print(calculation_poly(int(input("x 값 : ")), fx))
+print(fibonacci_loop(n))
+print(fibonacci_recursion(n))
+print(fibonacci_memo(n))
 
 
 
