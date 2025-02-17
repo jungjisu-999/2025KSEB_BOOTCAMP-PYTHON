9e6c2915e1c6db3db39252e48cfa38d8bcfadd33
unknown <deepblue@inha.ac.kr>
2025-02-17 11:00:16 +0900

    v0.4 day06 recursion

--- day06.py
+++ day06.py
@@ -1,23 +1,14 @@
-def is_even(n) -> bool:
+#8진수로 바꾸는 코드 작성
+
+def dec_oct(n) -> int:
+    if n == 0:
+        return ""
+    else:
+        return dec_oct(n // 8) + str(n % 8)
+        # return dec_oct(n // 2) + str(n % 2)
 
 
-
- # 짝수 판정 함수
- # :param n:판정할 정수
- # "return: 짝수면 True,  홀수면 False
-
-
-    if n % 2 == 0:
-        return True
-    return False
-
-a = 10
-b = 11
-print(a & b)
-print (a | b)
-print(a ^ b)
-
+##재귀함수 사용 재귀를 어디다가 사용했다는거임
 n = int(input())
-print(is_even(n))
-
-
+print(dec_oct(n))
+# print(n, oct(n))
